c101cb939e299275f4f6f57468b698b0b995469a
LeewellCaputol <LeewellC21@gmail.com>
2025-09-13 21:46:55 +0800

    Placed the prompts for OpenAI (Still untested)

--- src/Routes/openAi.ts
+++ src/Routes/openAi.ts
@@ -1,38 +1,12 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { middleWare } from "../Middleware/middleWare";
-import OpenAI from "openai";
-import dotenv from "dotenv";
-dotenv.config();
+import { mainPrompt } from "../Controllers/openAi/mainPrompt";
+import { brainBytesPrompts } from "../Controllers/openAi/brainBytesPrompts";
 
 const openAiRoute = express();
 
-const openai = new OpenAI({
-  apiKey: process.env.API_KEY,
-});
-
-openAiRoute.post(
-  "/evaluate",
-  middleWare,
-  async (req: Request, res: Response) => {
-    const { prompt } = req.body;
-
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: ` `,
-        },
-      ],
-    });
-    if (!response) {
-      return res.status(400).send({ message: "cant call" });
-    }
-
-    const reply = response.choices[0].message.content;
-    console.log(reply);
-    res.send({ response: reply });
-  }
-);
+//Still untested
+openAiRoute.post("/evaluate", middleWare, mainPrompt);
+openAiRoute.post("/evaluate", middleWare, brainBytesPrompts);
 
 export default openAiRoute;
